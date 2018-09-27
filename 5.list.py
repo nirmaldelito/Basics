ebedfcc72964d2e9a8abeb66f86107b7578f929d
Nirmal Sridhar <nirmalfez@gmail.com>
2018-09-27 11:05:43 +0530

    Added python basics

--- 5.list.py
+++ 5.list.py
@@ -13,13 +13,27 @@ def find(x):
 #c. Print true/false for the following question - “does it contain any duplicate elements?”
 
 
-if len(list) == (len(set(list))):
-    print("false,the list contains no duplicates")
-else:
-    print("True,the list contains duplicates")
+def dul(list):
+
+    if len(list) == (len(set(list))):
+        return False
+    else:
+        return False
+
+
+ans = dul(list)
+print(ans)
+
 #d. What would the output look like for you program for the following
 array = [12, 71, 92, 56, 121, 33, 89, 47, 25, 64, 100, 103, 2, 19, 96]
-x = 47
-output("yes")
-x = 99
-output("no")
+
+
+def candy(x):
+    if x in array:
+        print("yes")
+    else:
+        print("no")
+
+
+candy(47)
+candy(99)
