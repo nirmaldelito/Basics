3b674a6ae0ac2eaee13dcd68fcd7762392af79a1
Nirmal Sridhar <nirmalfez@gmail.com>
2018-09-27 11:33:15 +0530

    Added python basics

--- 2.multiples of 7.py
+++ 2.multiples of 7.py
@@ -1,9 +1,10 @@
-def mul(a,b):
-    sum = 0
-    for n in range(a,b):
+def mul(a, b):
+    pro = 0
+    for n in range(a, b):
         if n % 7 == 0:
-            sum += n
-    return(sum)
+            pro += n
+    return pro
 
-q = mul(100,150)
-print(q)
+
+ans = mul(1107, 21000)
+print(ans)
--- 3.odd numbers.py
+++ 3.odd numbers.py
@@ -1,6 +1,7 @@
-list =[]
-for odd in range(904,1100):
+lis = []
+
+for odd in range(904, 1100):
     if odd % 2 == 1:
-        list.append(odd)
-print(list)
-print("length=", len(list))
+        lis.append(odd)
+print(lis)
+print("length=", len(lis))
